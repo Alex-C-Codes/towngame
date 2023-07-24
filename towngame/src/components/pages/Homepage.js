95ffb76440f3f58d65e47671078183c6dee9de4e
Alex-C <alexcook1613@gmail.com>
2023-07-24 13:43:17 -0400

    updated naming of pathways in hompepage js

--- towngame/src/components/pages/Homepage.js
+++ towngame/src/components/pages/Homepage.js
@@ -3,14 +3,15 @@ import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function Homepage() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/TownDefense/build/TownDefense.loader.js",
-    dataUrl: "/TownDefense/build/TownDefense.data",
-    frameworkUrl: "/TownDefense/build/TownDefense.framework.js",
-    codeUrl: "/TownDefense/build/TownDefense.wasm",
+    loaderUrl: "/towndefense/build/towndefense.loader.js",
+    dataUrl: "/towndefense/build/towndefense.data",
+    frameworkUrl: "/towndefense/build/towndefense.framework.js",
+    codeUrl: "/towndefense/build/towndefense.wasm",
   });
   
   return (
     <div>
+      {/* <Unity unityProvider={unityProvider} /> */}
       <Unity unityProvider={unityProvider} style={{width:960, height:540}} />
       {/* <Unity unityProvider={unityProvider} width="100%" height="100%" /> */}
       {/* <Unity unityProvider={unityProvider} style={{width:1920, height:1080}} /> */}
